7f3b8c9b4f2fe5c172157e0b760c4a91256b0f60
sgurmani <ideegamesunity@gmail.com>
2026-08-25 16:33:42 +0200

    chore(release): bump version to 5.2.6

--- src/shared/components/main-sidebar/Footer.tsx
+++ src/shared/components/main-sidebar/Footer.tsx
@@ -123,7 +123,7 @@ const Footer: VFC = () => {
 
       <Box textAlign="center">
         <Text fontSize="md" fontWeight={700} color={Colors.DI_SERRIA}>
-          ver 5.2.5
+          ver 5.2.6
         </Text>
       </Box>
     </Flex>
